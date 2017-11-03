3bd5cfc389e7651c91341101804ce94e487daeeb
Yoni <ymekuria@gmail.com>
2017-11-03 16:12:14 -0700

    using desktoCapturer from Electron to get users screen sources

--- src/actions/record_actions.js
+++ src/actions/record_actions.js
@@ -1,7 +1,11 @@
-const { ipcRenderer } = window.electron;
+const { ipcRenderer, desktopCapturer } = window.electron;
 import { GET_SCREEN_SOURCES } from './types';
 
 export const getUserScreens = () => async => dispatch => {
 
+    let sources = await desktopCapturer.getSources({types: ['window', 'screen']});
+    console.log('sources: ', sources);
+
+    dispatch({ type: GET_SCREEN_SOURCES, payload: sources });
 
 };
