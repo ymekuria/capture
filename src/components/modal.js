aedf5c75118d1740e70b0613c215f40b6fd726bb
Yoni <ymekuria@gmail.com>
2017-11-16 17:00:06 -0800

    boiler plate for a reusable modal component

---
+++ src/components/modal.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Modal extends Component {
+  render() {
+    return (
+      <div>MODAL</div>
+      <div>MODAL</div>
+      <div>MODAL</div>
+      <div>MODAL</div>
+      <div>MODAL</div>
+      <div>MODAL</div>
+      <div>MODAL</div>
+    );
+  }
+}
+
+export default Modal;
